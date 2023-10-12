92f0bb8a6f0ffb93531b3264051e058dc8ad2fe1
Ashton Baker <mail@ashtonbaker.com>
2023-10-12 12:45:16 -0600

    Initial Commit

---
+++ EmacsDarkmodeNotifier/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  EmacsDarkmodeNotifier
+//
+//  Created by Ashton Scott Baker on 2023-10-12.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
